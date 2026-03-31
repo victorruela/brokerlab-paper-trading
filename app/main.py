69f7d5206a86d60a53598d33467d7e40e438d8aa
Victor Magalhães Ruela <victormruela@gmail.com>
2026-03-31 19:55:54 -0300

    Refactor main.py by removing unnecessary code

--- app/main.py
+++ app/main.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
-
-app = FastAPI(title="BrokerLab API")
-
-
-@app.get("/")
-def root():
-    return {"message": "BrokerLab API is running 🚀"}
-
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-from fastapi import FastAPI
 from app.core.database import engine, Base
+from app.models.user import User
 
 app = FastAPI(title="BrokerLab API")
 
-
-# cria o banco automaticamente
 Base.metadata.create_all(bind=engine)
 
 
