557d69995b6f4fceb1482bb406b32e1fa6426878
Victor Magalhães Ruela <victormruela@gmail.com>
2026-03-31 20:06:40 -0300

    Refactor import statement and reformat code

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base
-from app.models.user import User
+from app.models.user import User  # IMPORTANTE
 
 app = FastAPI(title="BrokerLab API")
 
 Base.metadata.create_all(bind=engine)
-
-
-@app.get("/")
-def root():
-    return {"message": "BrokerLab API is running 🚀"}
-
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
