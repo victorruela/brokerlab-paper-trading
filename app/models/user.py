e485e961f80686b91d84ebdc0a5a2ad1c0012123
Victor Magalhães Ruela <victormruela@gmail.com>
2026-03-31 19:52:59 -0300

    feat: create models module and add User entity

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    cash_balance = Column(Float, default=100000.0)
