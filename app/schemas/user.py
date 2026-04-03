e1cb0c22542672592e404e0b4cda4c622866fff3
Victor Magalhães Ruela <victormruela@gmail.com>
2026-04-03 11:56:06 -0300

    Add UserCreate and UserResponse schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel, EmailStr
 
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    cash_balance: float
+
+    class Config:
+        from_attributes = True
